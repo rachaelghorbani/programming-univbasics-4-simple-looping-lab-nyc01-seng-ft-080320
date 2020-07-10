c23031055b122bea829495a1f153a9b42e45553f
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-10 18:51:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,18 @@
 # Write your methods here
+def loop_message_five_times(message)
+  counter = 0 
+  while counter <= 5 do
+    puts message
+  end 
+end 
+
+def loop_message_n_times(message, number)
+  counter = 0 
+  while counter <= number do
+    puts message
+  end 
+end 
+
+def output_array(message, number)
+  
+end 
